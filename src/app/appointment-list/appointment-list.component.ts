28d4726e194ed152d3342175616d4a76567199da
Tomer Kedem <tompha1@hotmail.com>
2023-12-19 16:18:16 +0200

    First init

---
+++ src/app/appointment-list/appointment-list.component.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+import { Appointment } from '../models/appointment';
+
+@Component({
+  selector: 'app-appointment-list',
+  templateUrl: './appointment-list.component.html',
+  styleUrls: ['./appointment-list.component.css']
+})
+export class AppointmentListComponent {
+
+  newAppointmentTitle: String = ""
+  newAppointmentDate: Date = new Date()
+
+  appointments: Appointment [] = []
+
+  addAppointment(){
+    if(this.newAppointmentTitle.trim().length && this. newAppointmentDate){
+      let newAppointment: Appointment = {
+        id: Date.now(),
+        title: this.newAppointmentTitle,
+        date: this.newAppointmentDate
+      }
+      this.appointments.push(newAppointment)
+      this.newAppointmentTitle = ""
+      this.newAppointmentDate = new Date()    
+    }
+  }
+
+  deleteAppointment(index: number){
+    this.appointments.splice(index, 1)
+  }
+
+
+}
